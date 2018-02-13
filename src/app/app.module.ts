855773dc70bce9afa1ae64d81a092b7dad6aea34
Prem Sudheep Ramanadham <Prem@MacBook-Pro.local>
2018-02-12 21:49:28 -0500

    package.json update to the lastest node modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { SuperHeroesComponent } from './main/super-heroes/super-heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuperHeroesComponent
   ],
   imports: [
     BrowserModule,
